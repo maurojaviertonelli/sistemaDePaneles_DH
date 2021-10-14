4fe1da4b8fea23c7cbd0f06582c87d72f9b5be2e
unknown <tonellimaurojavier@gmail.com>
2021-10-13 22:47:17 -0300

    Vista Home

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const mainRoutes = require('./src/routes/main');
 
+app.use('/public', express.static(__dirname + '/public'));
+app.listen (3000, () => console.log("Servidor corriendo"));
 
+app.use('/', mainRoutes);
 
 app.set('view engine', 'ejs');
 app.set('views','./src/views');
